faf6d24e4aac6a57c3cb15601e52b7364aeddd36
jgabriel.tamay@gmail.com <jgabriel.tamay@gmail.com>
2020-06-06 22:08:53 -0300

    Updates

---
+++ precache-manifest.4713666852234fd0f2440df38e8a4e3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af90008d3b216434f6a8986263c4667",
+    "url": "/proyectoreact/index.html"
+  },
+  {
+    "revision": "ea52912dfcb28f19faa8",
+    "url": "/proyectoreact/static/css/main.0e67ca84.chunk.css"
+  },
+  {
+    "revision": "4197f256deded5a986d6",
+    "url": "/proyectoreact/static/js/2.a12ad9ac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/proyectoreact/static/js/2.a12ad9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea52912dfcb28f19faa8",
+    "url": "/proyectoreact/static/js/main.1bbb5fbb.chunk.js"
+  },
+  {
+    "revision": "fa8cf4c3cc7f83fad4e8",
+    "url": "/proyectoreact/static/js/runtime-main.b426fdf4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/proyectoreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
